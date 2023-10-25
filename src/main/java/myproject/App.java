b0982f20e7ccc4fa716b7c7cc17d5ce2b4ac14d5
Mani Charan <loka.m@northeastern.edu>
2023-10-25 05:53:20 -0400

    added code for getting RDS up and starting EC2 with user data

--- src/main/java/myproject/App.java
+++ src/main/java/myproject/App.java
@@ -8,10 +8,18 @@ import com.pulumi.aws.ec2.inputs.InstanceEbsBlockDeviceArgs;
 import com.pulumi.aws.inputs.GetAvailabilityZonesArgs;
 import com.pulumi.aws.outputs.GetAvailabilityZonesResult;
 import com.pulumi.aws.outputs.GetRegionResult;
+import com.pulumi.aws.rds.ParameterGroup;
+import com.pulumi.aws.rds.ParameterGroupArgs;
+import com.pulumi.aws.rds.SubnetGroup;
+import com.pulumi.aws.rds.SubnetGroupArgs;
+import com.pulumi.core.Output;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 public class App {
     public static void main(String[] args) {
@@ -46,6 +54,8 @@ public class App {
         String instanceType = config.require("instanceType");
         String volumeType = config.require("volumeType");
         String ec2Name = config.require("ec2Name");
+        String rdsSecurityGroupName = config.require("rdsSecurityGroupName");
+        int databasePort = config.requireInteger("databasePort");
 
         // Create a VPC
         var vpc = new Vpc(vpcName, VpcArgs.builder()
@@ -79,6 +89,44 @@ public class App {
                     .build());
 
         }
+
+        // Create a Security Group for RDS Instances
+        var rdsSecurityGroup = new SecurityGroup(rdsSecurityGroupName, SecurityGroupArgs.builder()
+                .vpcId(vpc.id())
+                .tags(Map.of("Name",rdsSecurityGroupName))
+                .build());
+
+        var rdsSecurityGroupRule = new SecurityGroupRule("allow EC2 on "+databasePort, SecurityGroupRuleArgs.builder()
+                .type("ingress")
+                .fromPort(3306)
+                .toPort(3306)
+                .protocol("tcp")
+                .sourceSecurityGroupId(securityGroup.id())
+                .securityGroupId(rdsSecurityGroup.id())
+                .build());
+
+        var outboundRule = new SecurityGroupRule("Outbound Rule", SecurityGroupRuleArgs.builder()
+                .type("egress")
+                .fromPort(0)
+                .toPort(0)
+                .protocol("-1")
+                .securityGroupId(securityGroup.id())
+                .cidrBlocks(destinationCidrPublic)
+                .build());
+
+        ParameterGroup dbParameterGroup = new ParameterGroup("rds-parameter-group", ParameterGroupArgs.builder()
+                .family("mariadb10.6")
+                .description("Custom parameter group for Mariadb")
+                .tags(Collections.singletonMap("Name", "MyDBParameterGroup"))
+                .build());
+
+        String rdsInstanceIdentifier = "webdb";
+        String rdsMasterUsername = "admin";
+        String rdsMasterPassword = "password";
+        String dbName = "webdb";
+        String storageType = "gp2";
+        int allocatedStorage = 20;
+        String instanceClass = "db.t3.micro";
 
 
         var availabilityZones = AwsFunctions.getAvailabilityZones(GetAvailabilityZonesArgs.builder().state("available").build());
@@ -123,6 +171,43 @@ public class App {
                     .gatewayId(igw.id())
                     .build());
 
+            // Create a subnet group for your RDS instance
+            SubnetGroup dbSubnetGroup = new SubnetGroup("my-db-subnet-group1", SubnetGroupArgs.builder()
+                    .subnetIds(Output.all(privateSubnets.stream().map(Subnet::id).collect(toList())))
+                    .description("My RDS Subnet Group")
+                    .name("my-db-subnet-group1")
+                    .build());
+            // Create the RDS instance
+            com.pulumi.aws.rds.Instance rdsInstance = new com.pulumi.aws.rds.Instance("myRDSInstance", com.pulumi.aws.rds.InstanceArgs.builder()
+                    .instanceClass(instanceClass)
+                    .allocatedStorage(allocatedStorage)
+                    .engine("mariadb")
+                    .engineVersion("10.6.14")
+                    .identifier(rdsInstanceIdentifier)
+                    .username(rdsMasterUsername)
+                    .password(rdsMasterPassword)
+                    .skipFinalSnapshot(true)
+                    .publiclyAccessible(false)
+                    .multiAz(false)
+                    .parameterGroupName(dbParameterGroup.name())
+                    .dbName(dbName)
+                    .port(databasePort)
+                    .vpcSecurityGroupIds(rdsSecurityGroup.id().applyValue(Collections::singletonList))
+                    .dbSubnetGroupName(dbSubnetGroup.name())
+                    .tags(Collections.singletonMap("Name", "MyRDSInstance"))
+                    .build());
+
+            Output<String> userDataScript =rdsInstance.address().applyValue(v -> String.format(
+                    "#!/bin/bash\n" +
+                            "echo 'export DB_User=%s' >> /etc/environment\n" +
+                            "echo 'export DB_Password=%s' >> /etc/environment\n" +
+                            "echo 'export DB_Host=%s' >> /etc/environment\n" +
+                            "echo 'export DB_Port=%s' >> /etc/environment\n"+
+                            "echo 'export DB_Database=%s' >> /etc/environment\n",
+
+                            rdsMasterUsername, rdsMasterPassword,v,databasePort,dbName
+            ));
+
             // Create EC2 instance
             var instance = new Instance(ec2Name, InstanceArgs.builder()
                     .ami(amiId)
@@ -137,7 +222,25 @@ public class App {
                     .vpcSecurityGroupIds(securityGroup.id().applyValue(Collections::singletonList))
                     .subnetId(publicSubnets.get(0).id())
                     .disableApiTermination(false)
+                    .userData(userDataScript)
                     .tags(Map.of("Name",ec2Name))
+                    .build());
+
+            var instance1 = new Instance("webapp2", InstanceArgs.builder()
+                    .ami(amiId)
+                    .instanceType(instanceType)
+                    .keyName(sshKeyName)
+                    .ebsBlockDevices(InstanceEbsBlockDeviceArgs.builder()
+                            .deleteOnTermination(true)
+                            .deviceName(deviceName)
+                            .volumeType(volumeType)
+                            .volumeSize(volume)
+                            .build())
+                    .vpcSecurityGroupIds(securityGroup.id().applyValue(Collections::singletonList))
+                    .subnetId(publicSubnets.get(0).id())
+                    .disableApiTermination(false)
+                    .userData(userDataScript)
+                    .tags(Map.of("Name","webapp2"))
                     .build());
 
             return null;
